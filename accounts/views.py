23e0b0d06487d00a3bae365ec4b63266b2f4c6bd
sdpark1422 <sdp1422@naver.com>
2019-03-04 22:05:51 +0900

    로그인/로그아웃 시 사용자 이름 보이기 기능 추가

--- accounts/views.py
+++ accounts/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render, redirect
 from django.contrib.auth.models import User # 유저에 대한 기능
 from django.contrib import auth # 계정에 대한 권한
+
 # Create your views here.
 
 def signup(request):
@@ -8,7 +9,7 @@ def signup(request):
         # 회원가입시 입력한 비밀번호와 재입력 비밀번호 확인하고 맞다면 로그인함수 실행
         if request.POST['password'] == request.POST['confirmpass']:
             user = User.objects.create_user( username=request.POST['username'], password=request.POST['password'])
-            auth.login(request, user)
+            auth.login(request, user)            
             return redirect('home')
     return render(request, 'signup.html')
 
@@ -19,20 +20,21 @@ def login(request):
         
         # 로그인을 시도할시 데이터베이스에 저장된 정보와 일치하는지 확인하고 결과를 user에 담음 (아니면 none)
         user = auth.authenticate(request, username=username, password=password)
-        
+                
         # 결과가 맞을시 로그인함수 실행. 아니면 메세지를 출력하며 거부.
         if user is not None:
-            auth.login(request, user)
+            auth.login(request, user)   
             return redirect('home')
         else:
             return render(request, 'login.html', {'error' : 'username or password is incorrect.'})
-    else:
+    else:        
         return render(request, 'login.html')
 
 def logout(request):
-    if request.method == 'POST':
-        auth.logout(request)
-        # 로그아웃하고 홈으로 돌아가기
+    if request.method == 'POST':        
+        # auth.logout(request)        
+        # # 로그아웃하고 홈으로 돌아가기        
         return redirect('home')
-    
-    return render(request, 'login.html')
+    else:
+        auth.logout(request)
+        return render(request, 'login.html')
--- blog/context_processors.py
+++ blog/context_processors.py
@@ -1,6 +1,14 @@
 from pyowm import OWM
+from django.contrib.auth.models import User # 유저에 대한 기능
+from django.contrib import auth # 계정에 대한 권한
 
-def get_weather(request):
+def get_weather(request):    
+    
+    username = auth.get_user(request)
+
+    if username.is_anonymous:
+        username = 'Guest'    
+
     owm = OWM(API_key='f74e57b358712ff9b0bae5f6d02ec271')
     obs = owm.weather_at_place('Seoul')
     obs = owm.weather_at_id(1835848)
@@ -9,4 +17,4 @@ def get_weather(request):
     w = obs.get_weather()
     wStatus = w.get_status()
     
-    return {'weather' : wStatus}
+    return {'weather' : wStatus, 'username':username}
