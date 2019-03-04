dbd6b6bd38ede3afb3c0ebfa573d5df2cfad08c9
sdpark1422 <sdp1422@naver.com>
2019-03-04 15:09:31 +0900

    페이지 네비게이터 수정

--- blog/views.py
+++ blog/views.py
@@ -22,8 +22,9 @@ def home(request):
     
     # request된 페이지를 얻어온뒤 return
     posts = paginator.get_page(page)
+    for_range = posts.paginator.page_range
 
-    return render(request, 'home.html', {'blogs' : blogs, 'posts' : posts})
+    return render(request, 'home.html', {'blogs' : blogs, 'posts' : posts, 'range':for_range})
 
 def detail(request, blog_id):
     blog_detail = get_object_or_404(Blog, pk = blog_id)
